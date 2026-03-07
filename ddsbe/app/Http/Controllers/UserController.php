0e92f032809a1177a0314729b512b4bd5915ee2d
JacRepo <parel.jacquelinejose@gmail.com>
2026-03-07 08:01:05 +0800

    Arrange some line
    Arrange some line

--- ddsbe/app/Http/Controllers/UserController.php
+++ ddsbe/app/Http/Controllers/UserController.php
@@ -21,8 +21,7 @@ class UserController extends Controller
     */
     public function successResponse($data, $code = 200)
     {
-        return response()->json(['data' => $data], 
-        $code);
+        return response()->json(['data' => $data], $code);
     }
 
     /**
